dfe1f9dbd057163b837841c43b8c5874dec41e87
CSR2017 <suganya@imanisoftware.com>
2024-07-24 17:25:26 -0500

    Update header

--- kafka/ed-fi-kafka-connect-transforms/src/test/java/org/edfi/kafka/connect/transforms/GenerateIndexFromDmsResourceTest.java
+++ kafka/ed-fi-kafka-connect-transforms/src/test/java/org/edfi/kafka/connect/transforms/GenerateIndexFromDmsResourceTest.java
@@ -15,10 +15,8 @@
  */
 
 /*
- *  SPDX-License-Identifier: Apache-2.0
- *  Licensed to the Ed-Fi Alliance under one or more agreements.
- *  The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
- *  See the LICENSE and NOTICES files in the project root for more information.
+ * SPDX-License-Identifier: Apache-2.0
+ * This file has been modified by the Ed-Fi Alliance.
  */
 
 package org.edfi.kafka.connect.transforms;
